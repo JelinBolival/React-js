4279a9d267335687704f0bf787b771b8a0d8e582
JelinBolival <jennietanunisha0317@gmail.com>
2025-01-20 09:11:34 +0530

    Add files via upload

--- http.js
+++ http.js
@@ -5,13 +5,9 @@ const server = http.createServer((req, res) => {
   // res.writeHead(200, { 'Content-Type': 'text/plain' });
   // res.end('Hello, this is a Node.js HTTP server!');
 
-  // res.writeHead(200, { 'Content-Type': 'text/plain' })
-  // res.end("Hello Js")
-
   const path = req.url
   const method = req.method
 
-  // console.log(new Date(), path, method);
 
   if (path.includes('/abc') && method == 'GET') {
     res.end('Hello Js')
